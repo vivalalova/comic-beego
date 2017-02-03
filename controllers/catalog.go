682916474e15bd3b5996e9d1f2f10be82ede6e76
Lova <vivalalova0@gmail.com>
2017-02-03 17:04:48 +0800

    catalog skip &limit

--- controllers/catalog.go
+++ controllers/catalog.go
@@ -3,6 +3,9 @@ package controllers
 import (
 	"comic-go/models"
 	"log"
+	"strconv"
+
+	"fmt"
 
 	"github.com/astaxie/beego"
 	"gopkg.in/mgo.v2/bson"
@@ -12,13 +15,39 @@ type CatalogController struct {
 	beego.Controller
 }
 
-func (this *CatalogController) Get() {
+// type CatalogParams struct {
+// 	limit    int
+// 	skip     int
+// 	category string
+// 	title    string
+// }
 
-	this.GetStrings()
+// func (this *string) toCatalogParams() CatalogParams {
+// 	return CatalogParams{}
+// }
+
+func (this *CatalogController) Get() {
+	limitString := this.GetString("limit")
+	limit, _ := strconv.Atoi(limitString)
+
+	if limit == 0 {
+		limit = 30
+	}
+
+	skipString := this.GetString("skip")
+	skip, _ := strconv.Atoi(skipString)
+
+	category := this.GetString("category")
+	title := this.GetString("title")
+	fmt.Println(category)
+	fmt.Println(title)
 
 	var results []models.Catalog
 
-	err := Catalog.Find(bson.M{}).Limit(30).All(&results)
+	query := bson.M{}
+	// query["category"] = "辛巴达的冒险"
+
+	err := Catalog.Find(query).Skip(skip).Limit(limit).All(&results)
 
 	if err != nil {
 		log.Fatal(err)
@@ -26,4 +55,5 @@ func (this *CatalogController) Get() {
 
 	this.Data["json"] = results
 	this.ServeJSON()
+
 }
